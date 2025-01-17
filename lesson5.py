cd8bd40a856b93fc73b6468d996e853fcb032cff
Ashraf-DIU <ashrafbialam17@ggmail.com>
2025-01-17 19:15:42 +0600

    Adding list and tuple

--- lesson5.py
+++ lesson5.py
@@ -1,30 +1,23 @@
-# c = 1
-# while c <= 5:
-#     print("Hello Loop", c)
-#     c += 1
-# print(c) 
+c = 1
+while c <= 5:
+    print("Hello Loop", c)
+    c += 1
+print(c) 
 
-# i = 1
-# while i <= 10:
-#     print(i)
-#     i += 1
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
-# # Multiplication Table
-# n =  int(input("Input any Number : "))
-# i = 1
-# while i <= 10:
-#     print(n, " x ", i, " = ",n*i)
-#     i += 1
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
 
-list = []
-len = int(input("Enter Index Number of List : "))
-n = 0
-while n < len:
-    element = int(input(f"Enter {n+1} Element : "))
-    list.append(element)
-    n += 1
-print("So the List : ",list)
-idx = 0
-while idx < len:
-    print(list[idx])
-    idx +=  1
+# Multiplication Table
+n =  int(input("Input any Number : "))
+i = 1
+while i <= 10:
+    print(n, " x ", i, " = ",n*i)
+    i += 1
+
--- list_tuple.py
+++ list_tuple.py
@@ -1,14 +1,30 @@
-list = []
-len = int(input("Enter Index Number of List : "))
-n = 0
-while n < len:
-    element = int(input(f"Enter {n+1} Element : "))
-    list.append(element)
-    n += 1
+# list = []
+# len = int(input("Enter Index Number of List : "))
+# n = 0
+# while n < len:
+#     element = int(input(f"Enter {n+1} Element : "))
+#     list.append(element)
+#     n += 1
 
-print("So the List : ",list)
+# print("So the List : ",list)
 
-idx = 0
-while idx < len: # list traverse
-    print(list[idx])
-    idx += 1
+# idx = 0
+# while idx < len: # list traverse
+#     print(list[idx])
+#     idx += 1
+
+# Tuple
+# Taking input
+a = input("Enter elements separated by spaces: ").split()
+
+# Converting list to tuple
+t = tuple(a)
+print("So the tuple is : ",t)
+x = 3
+i = 0
+while i < len(t):
+    if(t[i] == x):
+        print("Found at index ",i)
+    else:
+        print("Finding...")
+    i += 1
